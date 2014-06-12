69fb3c81fb5cf2c74820cc5cf55bafed86923139
Zack Elliott <hubridnoxx@gmail.com>
2014-06-12 15:29:06 -0400

    some personal website stuff

--- js/global.js
+++ js/global.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
         scrollTop: $(".work-content").offset().top
     }, 500);
   });
+  $(".projects-menu").click(function() {
+    $("html, body").animate({
+        scrollTop: $(".projects-content").offset().top
+    }, 500);
+  });
 });
