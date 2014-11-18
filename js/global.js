78b467d5e71ea8d38db4c9e62d051aef6cb6184e
Zack Elliott <hubridnoxx@gmail.com>
2014-11-17 20:36:22 -0500

    Asdas

--- js/global.js
+++ js/global.js
@@ -1,17 +1,44 @@
 $(document).ready(function() {
+  $('.about-menu').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500, function() {
+      checkMenu(window.pageYOffset)
+    });
+  });
   $('.work-menu').click(function() {
     $('html, body').animate({
         scrollTop: $('.work-content').offset().top
-    }, 500);
+    }, 500, function() {
+      checkMenu(window.pageYOffset)
+    });
   });
   $('.projects-menu').click(function() {
     $('html, body').animate({
         scrollTop: $('.projects-content').offset().top
-    }, 500);
+    }, 500, function() {
+      checkMenu(window.pageYOffset)
+    });
   });
 
-  $('.menu > div').click(function() {
-    $('.menu > div').removeClass('on');
-    $(this).addClass('on');
+  $(window).on('scroll', function() {
+    checkMenu(window.pageYOffset);
   });
+
+  function checkMenu(scrollPos) {
+    var scrollWork = $('.work-content').offset().top,
+        scrollProj = $('.projects-content').offset().top;
+
+    if(scrollPos >= scrollProj) {
+      $('.menu > div').removeClass('on');
+      $('.projects-menu').addClass('on');
+    } else if(scrollPos >= scrollWork && scrollPos < scrollProj) {
+      $('.menu > div').removeClass('on');
+      $('.work-menu').addClass('on');
+    } else if(scrollPos < scrollWork) {
+      $('.menu > div').removeClass('on');
+      $('.about-menu').addClass('on');
+    }
+  }
+
 });
